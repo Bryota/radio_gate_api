e0e7f926dca1aea2730fe27af37db7ad9df26e51
sgw-ryota <ryota-segawe@sekawamshinoMBP.lan>
2022-04-10 20:40:35 +0900

    不要なファイルを削除、修正

--- database/migrations/2014_10_12_000000_create_listeners_table.php
+++ database/migrations/2014_10_12_000000_create_listeners_table.php
@@ -13,9 +13,18 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('listeners', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('last_name')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name_kana')->nullable();
+            $table->string('first_name_kana')->nullable();
+            $table->string('radio_name')->nullable();
+            $table->integer('post_code')->nullable();
+            $table->string('prefecture')->nullable();
+            $table->string('city')->nullable();
+            $table->string('house_number')->nullable();
+            $table->integer('tel')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -31,6 +40,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('listeners');
     }
 };
