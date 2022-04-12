31b3df92d1f125d4e873f2ceae56da293583bfc5
sgw-ryota <ryota-segawe@sekawamshinoMBP.lan>
2022-04-12 21:08:54 +0900

    モデル＆マイグレーションファイル作成

---
+++ app/DataProviders/Models/RadioProgram.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\DataProviders\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RadioProgram extends Model
+{
+    use HasFactory;
+}
