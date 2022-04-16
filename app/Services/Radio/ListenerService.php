8e3f203b1d113307494ae49d6b41f9eabef5a09c
sgw-ryota <ryota-segawe@sekawamshinoMBP.lan>
2022-04-17 00:36:41 +0900

    リスナー登録API実装

---
+++ app/Services/Radio/ListenerService.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * リスナー用の機能関連のビジネスロジック
+ *
+ * リスナーのアクションに関連する
+ *
+ * @author s_ryota sryotapersian@gmail.com
+ * @version 1.0
+ * @copyright 2022 Ryota Segawa
+ */
+
+namespace App\Services\Radio;
+
+use App\DataProviders\Models\Listener;
+use App\DataProviders\Repositories\ListenerRepository;
+use App\Http\Requests\ListenerRequest;
+
+/**
+ * リスナー用のサービスクラス
+ *
+ * @package App\Services
+ * @version 1.0
+ */
+class ListenerService
+{
+    /**
+     * @var ListenerRepository $listener ListenerRepositoryインスタンス
+     */
+    private $listener;
+
+    /**
+     * コンストラクタ
+     *
+     * @param ListenerRepository $listener ListenerRepositoryインスタンス
+     */
+    public function __construct(ListenerRepository $listener)
+    {
+        $this->listener = $listener;
+    }
+
+    /**
+     * リスナー登録
+     * 
+     * @param ListenerRequest $request リスナー登録用のリクエストデータ
+     * @return Listener 登録されたリスナー情報
+     */
+    public function CreateListener(ListenerRequest $request): Listener
+    {
+        $listener = $this->listener->CreateListener($request);
+        return $listener;
+    }
+}
