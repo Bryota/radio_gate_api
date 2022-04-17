4bf18d83eb7a3d001539b25945556bb8b37a8f79
sgw-ryota <ryota-segawe@sekawamshinoMBP.lan>
2022-04-17 22:07:49 +0900

    リレーション設定

--- app/DataProviders/Models/MessageTemplate.php
+++ app/DataProviders/Models/MessageTemplate.php
@@ -4,6 +4,7 @@ namespace App\DataProviders\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MessageTemplate extends Model
 {
@@ -19,4 +20,14 @@ class MessageTemplate extends Model
         'content',
         'listener_id'
     ];
+
+    /**
+     * リスナー用リレーション
+     * 
+     * @return BelongsTo
+     */
+    public function listener(): BelongsTo
+    {
+        return $this->belongsTo(Listener::class);
+    }
 }
