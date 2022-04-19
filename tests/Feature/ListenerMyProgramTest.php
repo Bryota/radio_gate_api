4ed4fe32551a946db23a8f5d610297ca2ed11120
sgw-ryota <ryota-segawe@sekawamshinoMBP.lan>
2022-04-19 21:40:07 +0900

    マイ番組作成APIのテスト追加

---
+++ tests/Feature/ListenerMyProgramTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\DataProviders\Models\ListenerMyProgram;
+use Tests\TestCase;
+
+class ListenerMyProgramTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setup(): void
+    {
+        parent::setUp();
+
+        $this->listener = $this->loginAsListener();
+    }
+
+    /**
+     * @test
+     * App\Http\Controllers\ListenerMyProgramController@store
+     */
+    public function マイ番組が作成できる()
+    {
+        $response = $this->postJson('api/listener_my_programs', ['program_name' => 'テストマイ番組', 'email' => 'test@example.com']);
+
+        $response->assertStatus(201)
+            ->assertJson([
+                'message' => 'マイ番組が作成されました。'
+            ]);
+
+        $listener_my_program = ListenerMyProgram::first();
+        $this->assertEquals('テストマイ番組', $listener_my_program->program_name);
+        $this->assertEquals('test@example.com', $listener_my_program->email);
+        $this->assertEquals($this->listener->id, $listener_my_program->listener_id);
+        $this->assertEquals($this->listener->name, $listener_my_program->Listener->name);
+    }
+
+    /**
+     * @test
+     * App\Http\Controllers\ListenerMyProgramController@store
+     */
+    public function マイ番組作成に失敗する（名前関連）()
+    {
+        $response1 = $this->postJson('api/listener_my_programs', ['program_name' => '', 'email' => 'test@example.com']);
+        $response1->assertStatus(400)
+            ->assertJson([
+                'errors' => [
+                    'program_name' => [
+                        '番組名を入力してください。'
+                    ]
+                ]
+            ]);
+
+        $response2 = $this->postJson('api/listener_my_programs', ['program_name' => str_repeat('あ', 151), 'content' => 'こんにちは！　いつも楽しく聴いています。', 'listener_id' => $this->listener->id]);
+        $response2->assertStatus(400)
+            ->assertJson([
+                'errors' => [
+                    'program_name' => [
+                        '番組名は150文字以下で入力してください。'
+                    ]
+                ]
+            ]);
+
+        $this->assertEquals(0, ListenerMyProgram::count());
+    }
+
+    /**
+     * @test
+     * App\Http\Controllers\ListenerMyProgramController@store
+     */
+    public function マイ番組作成に失敗する（メールアドレス関連）()
+    {
+        $response1 = $this->postJson('api/listener_my_programs', ['program_name' => 'テストマイ番組', 'email' => '']);
+        $response1->assertStatus(400)
+            ->assertJson([
+                'errors' => [
+                    'email' => [
+                        '番組メールアドレスを入力してください。'
+                    ]
+                ]
+            ]);
+
+        $this->postJson('api/listener_my_programs', ['program_name' => 'テストマイ番組', 'email' => 'test@example.com']);
+        $response2 = $this->postJson('api/listener_my_programs', ['program_name' => 'テストマイ番組', 'email' => 'test@example.com']);
+        $response2->assertStatus(400)
+            ->assertJson([
+                'errors' => [
+                    'email' => [
+                        '番組メールアドレスは既に保存されています。'
+                    ]
+                ]
+            ]);
+
+        $this->assertEquals(1, ListenerMyProgram::count());
+    }
+}
