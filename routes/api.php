ed2459cc226cf5713775b9a06a748b06d5982324
sgw-ryota <ryota-segawe@sekawamshinoMBP.lan>
2022-04-21 20:05:36 +0900

    ルーティング追加

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\ListenerController;
 use App\Http\Controllers\RadioStationController;
 use App\Http\Controllers\RadioProgramController;
 use App\Http\Controllers\ProgramCornerController;
@@ -22,6 +23,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [RegisterController::class, 'create'])->name('register');
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('listeners', ListenerController::class);
     Route::apiResource('radio_stations', RadioStationController::class);
     Route::apiResource('radio_programs', RadioProgramController::class);
     Route::apiResource('program_corners', ProgramCornerController::class);
