40065fbb1cadd2808490d5ca4880ebf8d7578ecf
sgw-ryota <ryota-segawe@sekawamshinoMBP.lan>
2022-04-22 22:35:36 +0900

    投稿メッセージ用のモデルとマイグレーション追加

---
+++ app/DataProviders/Models/ListenerMessage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\DataProviders\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ListenerMessage extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_04_22_221514_create_listener_messages_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listener_messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('radio_program_id')->nullable()->nullOnDelete()->constrained('radio_programs');
+            $table->foreignId('program_corner_id')->nullable()->nullOnDelete()->constrained('program_corners');
+            $table->foreignId('listener_my_program_id')->nullable()->nullOnDelete()->constrained('listener_my_programs');
+            $table->foreignId('my_program_corner_id')->nullable()->nullOnDelete()->constrained('my_program_corners');
+            $table->foreignId('listener_id')->nullOnDelete('cascade')->constrained('listeners');
+            $table->string('subject');
+            $table->string('content');
+            $table->string('radio_name');
+            $table->datetime('posted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listener_messages');
+    }
+};
