a9a7e9cb34c2788101c90fa187aaa937d63ab4e3
sgw-ryota <ryota-segawe@sekawamukufutoshinoMacBook-Pro.local>
2022-04-24 23:47:00 +0900

    パスワード再設定用のメール送信APIの実装

---
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ForgotPasswordRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+
+class ForgotPasswordController extends Controller
+{
+    /**
+     * パスワード再設定メール送信
+     * 
+     * @param ForgotPasswordRequest $request パスワード再設定メール送信用リクエスト
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendResetLinkEmail(ForgotPasswordRequest $request)
+    {
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        if ($status === Password::RESET_LINK_SENT) {
+            return response()->json([
+                'messege' => 'パスワード再設定用のメールを送信しました。'
+            ], 200, [], JSON_UNESCAPED_UNICODE);
+        } else {
+            return response()->json([
+                'messege' => 'パスワード再設定用のメールの送信に失敗しました。'
+            ], 500, [], JSON_UNESCAPED_UNICODE);
+        }
+    }
+}
